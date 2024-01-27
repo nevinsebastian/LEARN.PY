caab96dfa72552588481418ff8cacce92c385ecf
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-01-27 07:04:22 +0530

    maain

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ message = 'Hello kunna myre' #we can store data in variable in python
 #print(message[1])#to print a lthingfrom  a string we can specify the index
 #print(message[0:5])# we can specify where to start and end and first number is incluside and the other is exclusive if first is nothing then it will be considerd as min and if last is empty the considers as max
  
-print(message.lower()) #lowe 
-print(message.upper()) #capitqal
-print(message.count("Hello")) #to count it ,it is casse sensitive 
+#print(message.lower()) #lowe 
+#print(message.upper()) #capitqal
+#print(message.count("Hello")) #to count it ,it is casse sensitive 
+
+print(message.find("l")) #to find the index of a thing
