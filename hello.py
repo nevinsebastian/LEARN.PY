2d99b4e9a6e0979482af6a081c409b319299559b
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-02-01 20:17:12 +0530

    maain

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@
 #message = 'Hello kunna myre' #we can store data in variable in python.
 #print(message) #we can print it.
 #print(len(message)) #we can print the length of the thing inside of a varuable witht len().
-#print(message[1])#to print a lthingfrom  a string we can specify the index
+#print(message[1])#to print a lthingfrom  a string we can specify the index.
 #print(message[0:5])# we can specify where to start and end and first number is incluside and the other is exclusive if first is nothing then it will be considerd as min and if last is empty the considers as max
  
 #print(message.lower()) #lowe 
