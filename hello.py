0fd76a14bdc8e3c9d5d4526c854d39e8849e012f
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-02-01 20:16:22 +0530

    maain

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #print('hello myre') #to print a plane text 
 
-#message = 'Hello kunna myre' #we can store data in variable in python
+#message = 'Hello kunna myre' #we can store data in variable in python.
 #print(message) #we can print it
 #print(len(message)) #we can print the length of the thing inside of a varuable witht len()
 #print(message[1])#to print a lthingfrom  a string we can specify the index
