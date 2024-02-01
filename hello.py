5ceb30eea152a8422ea957c6fef78ae7d4bbdee4
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-02-01 20:17:47 +0530

    maain

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@
 #print(message[0:5])# we can specify where to start and end and first number is incluside and the other is exclusive if first is nothing then it will be considerd as min and if last is empty the considers as max.
  
 #print(message.lower()) #lowe .
-#print(message.upper()) #capitqal
+#print(message.upper()) #capitqal.
 #print(message.count("Hello")) #to count it ,it is casse sensitive 
 
 #print(message.find("l")) #to find the index of a thing
